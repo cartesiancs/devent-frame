917935f4e2a288ced1b4a2c677bc3fd1787381bb
Dipokal HHJ <hhj@devent.kr>
2022-01-26 11:28:31 +0900

    refactor: change HTML template
    + EJS 에서 handlebars로 변경

--- loaders/express.js
+++ loaders/express.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import { engine } from 'express-handlebars';
+
 
 import apiRouter from '../backend/api.js';
 import mainRouter from '../frontend/routes/main.js';
 
 export async function init (app) {
+    app.engine("hbs",
+        engine({
+            extname: "hbs",
+            defaultLayout: false
+        })
+    );
     app.set('trust proxy', 1);
-    app.set('view engine','ejs');
+    app.set("view engine", "hbs");    
     app.set('views','./frontend/views');
     app.disable('x-powered-by');
     
