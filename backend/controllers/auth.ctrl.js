41a6a2a121ce58d77bca55609d85575fd5c7d84d
Dipokal HHJ <hhj@devent.kr>
2022-02-10 00:05:29 +0900

    refactor: apply snake_case to variable name
    + 변수 이름 스네이크 문법 통일
    + package.json 깃허브 URL 수정

--- backend/controllers/auth.ctrl.js
+++ backend/controllers/auth.ctrl.js
@@ -18,8 +18,8 @@ export async function login (req, res) {
         let result = await comparePassword(user_pw, data.user_pw)
         
         if (result.status == 1 && data.user_auth == 1) {
-            let createdToken = await grantToken(user_id);
-            res.status(200).json({status:1, token:createdToken})
+            let created_token = await grantToken(user_id);
+            res.status(200).json({status:1, token:created_token})
         } else if (data.user_auth == 0) {
             res.status(401).json({status:-1})
         } else {
--- backend/controllers/feed.ctrl.js
+++ backend/controllers/feed.ctrl.js
@@ -18,13 +18,11 @@ export async function getFeed (req, res) {
 }
 
 export async function getFeedRange (req, res) {
+    let multiple_range = 10;
+    let get_start = Number(req.query.start);
 
-    let getRange = 10;
-    let queryStart = req.query.start;
-
-    let start = Number.isInteger(queryStart) == true ? queryStart : 0
-    let end = start+getRange;
-
+    let start = Number.isInteger(get_start) == true ? get_start : 0
+    let end = start+multiple_range;
 
     let data = await getFeedsRange({ start, end })
 
@@ -32,22 +30,21 @@ export async function getFeedRange (req, res) {
         res.status(200).json({data:data.result})
     } else {
         res.status(404).json({data:'', msg:'Not Found'})
-
     }
 }
 
 export async function insertFeed (req, res) {
     let token = req.headers['x-access-token'];
-    var now = dayjs();
+    let now = dayjs();
 
     let content = sanitizeHtml(req.body.content);
     let owner = await transformTokentoUserid(token);
     let date = now.format("YYYY.MM.DD.HH.mm.ss"); 
     let type = 1;
 
-    let insertData = { content, owner, date, type };
+    let insert_data = { content, owner, date, type };
 
-    let data = await insertFeedData(insertData)
+    let data = await insertFeedData(insert_data)
 
     if (data.status == 1) {
         res.status(200).json({status:1})
@@ -63,9 +60,7 @@ export async function deleteFeed (req, res) {
     let feed_idx = req.params.idx;
     let owner = await transformTokentoUserid(token);
 
-
     let check = { feed_idx, owner };
-
     let data = await deleteFeedData(check)
 
     if (data.status == 1) {
@@ -82,9 +77,9 @@ export async function updateFeed (req, res) {
     let feed_update_content = req.body.content;
     let owner = await transformTokentoUserid(token);
 
-    let updateData = { feed_update_idx, feed_update_content, owner };
+    let update_data = { feed_update_idx, feed_update_content, owner };
 
-    let data = await updateFeedData(updateData)
+    let data = await updateFeedData(update_data)
 
     if (data.status == 1) {
         res.status(200).json({status:1})
