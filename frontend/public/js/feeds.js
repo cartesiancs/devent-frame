5e21d44bf8cf5d3da1274906073b667731edb52e
Dipokal HHJ <hhj@devent.kr>
2022-03-25 16:42:22 +0900

    fix: change popup message
    + 로그인 메시지 변경 (-요)

--- frontend/public/js/feeds.js
+++ frontend/public/js/feeds.js
@@ -1,6 +1,5 @@
 
-async function getSendFeed(idx) {
-
+async function getFeed(idx) {
     let response = await fetch("/api/feeds/"+idx, {
         method: "GET"
     });
@@ -9,15 +8,8 @@ async function getSendFeed(idx) {
     return data;
 }
 
-async function getFeed(idx) {
-    let data = await getSendFeed(idx)
-    
-    console.log(data)
-}
 
-
-async function getSendFeedRange(start) {
-
+async function getFeedRange(start) {
     let response = await fetch(`/api/feeds?start=${start}`, {
         method: "GET",
         headers: {
@@ -29,44 +21,14 @@ async function getSendFeedRange(start) {
     return data;
 }
 
-async function getFeedRange(start) {
-    let data = await getSendFeedRange(start)
-    return data
-}
-
-
-async function insertSendFeed(content) {
-
+async function insertFeed(content) {
     let response = await fetch("/api/feeds", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
             "x-access-token": getToken('user')
-
         },
         body: `content="${content}"`
-
-    });
-
-    let data = response.json();
-    return data;
-}
-
-async function insertFeed(content) {
-    let data = await insertSendFeed(content)
-    
-    console.log(data)
-}
-
-async function deleteSendFeed(idx) {
-
-    let response = await fetch("/api/feeds/"+idx, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "x-access-token": getToken('user')
-        }
-
     });
 
     let data = response.json();
@@ -74,13 +36,19 @@ async function deleteSendFeed(idx) {
 }
 
 async function deleteFeed(idx) {
-    let data = await deleteSendFeed(idx)
-    
-    console.log(data)
+    let response = await fetch("/api/feeds/"+idx, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-access-token": getToken('user')
+        }
+    });
+
+    let data = response.json();
+    return data;
 }
 
-async function updateSendFeed(idx, content) {
-
+async function updateFeed(idx, content) {
     let response = await fetch("/api/feeds/"+idx, {
         method: "PUT",
         headers: {
@@ -92,10 +60,4 @@ async function updateSendFeed(idx, content) {
 
     let data = response.json();
     return data;
-}
-
-async function updateFeed(idx, content) {
-    let data = await updateSendFeed(idx, content)
-    
-    console.log(data)
 }
--- frontend/public/js/users.js
+++ frontend/public/js/users.js
@@ -14,47 +14,77 @@ async function userSendSignup(user) {
 }
 
 async function userSignup() {
-    let user_id = btoa(document.querySelector("#Username").value);
-    let user_pw = btoa(document.querySelector("#Password").value);
-    let user_email = btoa(document.querySelector("#Email").value);
-
-    let user = {user_id, user_pw, user_email}
+    try {
+        let user_id = btoa(document.querySelector("#Username").value);
+        let user_pw = btoa(document.querySelector("#Password").value);
+        let user_email = btoa(document.querySelector("#Email").value);
     
-    let data = await userSendSignup(user);
-    if (data.status == 1) {
-        Cookies.set('user', data.token)
+        if (user_id == '' || user_pw == '' || user_email == '') {
+            return Swal.fire({
+                icon: 'error',
+                title: '입력칸을 확인해주세요',
+                showConfirmButton: false,
+                heightAuto: false,
 
-        Swal.fire({
-            icon: 'success',
-            title: '가입 성공',
-            showConfirmButton: false,
-            timer: 1000
-        })
-        setTimeout(() => {
-            location.href = '/'
-        }, 1200);
-    } else if (data.status == 2) { // 비번 8자리
+                timer: 1500
+            })
+        }
+    
+        let user = {user_id, user_pw, user_email}
+        
+        let data = await userSendSignup(user);
+        if (data.status == 1) {
+            Cookies.set('user', data.token)
+    
+            Swal.fire({
+                icon: 'success',
+                title: '가입 성공',
+                showConfirmButton: false,
+                heightAuto: false,
+
+                timer: 1300
+            })
+            setTimeout(() => {
+                location.href = '/'
+            }, 1200);
+        } else if (data.status == 2) { // 비번 8자리
+            Swal.fire({
+                icon: 'error',
+                title: '바밀번호는 8자리 이상이여야 해요',
+                showConfirmButton: false,
+                heightAuto: false,
+
+                timer: 1500
+            })
+        } else if (data.status == 5) { // 특수문자
+            Swal.fire({
+                icon: 'error',
+                title: '아이디에 특수문자는 입력할 수 없어요',
+                showConfirmButton: false,
+                heightAuto: false,
+
+                timer: 1500
+            })
+        } else if (data.status == 0) {
+            Swal.fire({
+                icon: 'error',
+                title: '사용 불가한 아이디 또는 이메일이에요',
+                showConfirmButton: false,
+                heightAuto: false,
+                timer: 1500
+            })
+        }
+    } catch (error) {
         Swal.fire({
             icon: 'error',
-            title: '바밀번호는 8자리 이상이여야 합니다',
+            title: '에러가 발생했어요',
             showConfirmButton: false,
-            timer: 1000
-        })
-    } else if (data.status == 5) { // 특수문자
-        Swal.fire({
-            icon: 'error',
-            title: '아이디에 특수문자는 기입하실 수 없습니다',
-            showConfirmButton: false,
-            timer: 1000
-        })
-    } else if (data.status == 0) {
-        Swal.fire({
-            icon: 'error',
-            title: '중복된 사용자가 있거나 사용 불가한 아이디 입니다.',
-            showConfirmButton: false,
-            timer: 1000
+            heightAuto: false,
+
+            timer: 1500
         })
     }
+
 }
 
 async function userSendLogin(user) {
@@ -86,7 +116,9 @@ async function userLogin() {
             icon: 'success',
             title: '로그인 성공',
             showConfirmButton: false,
-            timer: 1000
+            heightAuto: false,
+
+            timer: 1300
         })
         setTimeout(() => {
             location.href = '/'
@@ -94,16 +126,20 @@ async function userLogin() {
     } else if (data.status == -1) {
         Swal.fire({
             icon: 'error',
-            title: '탈퇴한 회원이거나 권한이 없습니다',
+            title: '탈퇴한 회원이거나 권한이 없어요',
             showConfirmButton: false,
-            timer: 1000
+            heightAuto: false,
+
+            timer: 1500
         })
     } else {
         Swal.fire({
             icon: 'error',
-            title: '로그인 실패',
+            title: '로그인 정보가 맞지 않아요',
             showConfirmButton: false,
-            timer: 1000
+            heightAuto: false,
+
+            timer: 1500
         })
     } 
 }
@@ -140,9 +176,11 @@ async function userDelete() {
     } else if (data.status == -1) {
         Swal.fire({
             icon: 'error',
-            title: '권한이 없습니다',
+            title: '권한이 없어요',
             showConfirmButton: false,
-            timer: 1000
+            heightAuto: false,
+
+            timer: 1500
         })
     } else {
         Swal.fire({
